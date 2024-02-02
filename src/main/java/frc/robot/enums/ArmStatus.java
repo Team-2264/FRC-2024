813e83de36b69b1cb440678e82863a5c9f313002
Beck Bjella <beckbjella@gmail.com>
2024-02-01 23:18:45 -0600

    add ArmStatus enum

---
+++ src/main/java/frc/robot/enums/ArmStatus.java
@@ -0,0 +1,10 @@
+package frc.robot.enums;
+
+/**
+ * Enum for the arm status. Represents what the arm is currently doing.
+ * 
+ */
+public enum ArmStatus {
+    INTAKE, HOME, AMP, SHOOT
+    
+}
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.EndEffector;
 import frc.robot.Shoulder;
+import frc.robot.enums.ArmStatus;
 
 /**
  * Subystem for controlling the arm.
@@ -11,6 +12,8 @@ import frc.robot.Shoulder;
 public class Arm extends SubsystemBase {
     private final Shoulder shoulder;
     private final EndEffector endEffector;
+    
+    private ArmStatus status;
 
     /**
      * Constructs a new Arm instance.
@@ -18,6 +21,18 @@ public class Arm extends SubsystemBase {
     public Arm() {
         shoulder = new Shoulder();
         endEffector = new EndEffector();
+
+        status = ArmStatus.HOME;
+        
+    }
+
+    /**
+     * Sets the state of the arm.
+     * 
+     * @param status The new state of the arm.
+     */
+    public void setState(ArmStatus status) {
+        this.status = status;
         
     }
 
